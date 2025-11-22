f168f4486faf1d921ae268e8d4c186cd32136be1
Tanmay Shinde <tanmaysm2005@gmail.com>
2025-11-22 10:28:23 +0530

    backend-latest

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'inventory',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quicktrace_db',
+        'USER': 'postgres',
+        'PASSWORD': 'tanmay',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

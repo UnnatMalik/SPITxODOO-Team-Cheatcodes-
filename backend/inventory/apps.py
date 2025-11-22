0b115b3906d30379e8115e47e1ed0f072803dd45
UnnatMalik <parvinder221978@gmail.com>
2025-11-22 09:50:51 +0530

    Backend

--- backend/inventory/apps.py
+++ backend/inventory/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuicktraceapiConfig(AppConfig):
+class InventoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'QuickTraceAPI'
+    name = 'inventory'
